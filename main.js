f42c723b768fa08fbf17372099972940361f8f08
Roscoe Clark <roscoe.clark9@gmail.com>
2020-03-05 14:57:23 -0500

    boxes change color on click clear works

--- main.js
+++ main.js
@@ -2,5 +2,16 @@ console.log('hi there');
 
 $( document ).ready(function() {
   console.log('hi there on dom ready');
+  $('.gameboard').on('click', '.gamePlace',function() {
+    $(this).css('background-color', 'pink');
+  });
+  //clear
+  
+    $('body').on('click', '.btnClear', function(){
+      console.log('hi ready');
 
+      location.reload();
+    });
+  
 });
+
